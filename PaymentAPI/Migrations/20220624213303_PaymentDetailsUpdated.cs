eec8dc26b0404027514622492145284cd6dcbde4
Nikola Vujicic <nikolavujicic.nfinnova@outlook.com>
2022-07-16 22:57:51 +0200

    Add project files.

---
+++ PaymentAPI/Migrations/20220624213303_PaymentDetailsUpdated.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class PaymentDetailsUpdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SecurityCode",
+                table: "PaymentDetails",
+                type: "nvarchar(4)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(3)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "SecurityCode",
+                table: "PaymentDetails",
+                type: "nvarchar(3)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(4)",
+                oldNullable: true);
+        }
+    }
+}
